e719f156c0ae80e0605cc4b2c070894d24ccfaf9
ender <68495763+tookender@users.noreply.github.com>
2025-03-08 21:24:41 +0100

    🩷 Add some color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Button } from "@heroui/react";
-import { BiBook } from "react-icons/bi";
+import { Button,Link } from "@heroui/react";
+import { BiBook, } from "react-icons/bi";
 import { SiDiscord } from "react-icons/si";
 import { TbArrowRight } from "react-icons/tb";
 
@@ -15,31 +15,39 @@ export default function Home() {
       </button>
 
       <div className="flex flex-col items-center justify-center">
-        <h1 className="mt-4 text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:text-6xl lg:leading-[1.1]">
-          The Discord bot for giveaways.
-        </h1>
+        <div className="mt-4 flex flex-col sm:flex-row items-center sm:gap-2 font-bold leading-tight lg:leading-[1.1] tracking-tighter text-4xl md:text-5xl lg:text-6xl">
+          <h1>The Discord bot for</h1>
+          <div className="flex flex-row text-center">
+            <p className="bg-gradient-to-r from-[#f97ebb] to-[#ff3888] bg-clip-text text-transparent">giveaways</p>
+            <p>.</p>
+          </div>
+        </div>
 
-        <p className="mt-2 mx-12 text-center text-lg text-neutral-400 md:max-w-[520px]">
+        <p className="mt-2 sm:mt-4 mx-12 text-center text-base sm:text-lg text-neutral-400 md:max-w-[520px]">
           Bao Bun is a very customizable giveaways bot for your Discord server
           designed with ease-of-use in mind.
         </p>
       </div>
 
-      <div className="mt-12 gap-4 flex flex-row items-center justify-center">
-        <a target="_blank" href="https://discordapp.com/oauth2/authorize?client_id=1340917960621162557&scope=bot+applications.commands&permissions=8">
-          <Button
-            variant="solid"
-            startContent={<SiDiscord className="text-lg" />}
-          >
-            Add to Discord
+      <div className="mt-8 sm:mt-12 gap-4 flex flex-row items-center justify-center">
+        {/* <ButtonGroup variant="solid"> */}
+        <Button
+          as={Link}
+          variant="solid"
+          target="_blank"
+          href="https://discordapp.com/oauth2/authorize?client_id=1340917960621162557&scope=bot+applications.commands&permissions=8"
+          startContent={<SiDiscord className="text-lg" />}
+        >
+          Add to Discord
+        </Button>
+
+          {/* <Button isIconOnly onPress={onOpen}>
+            <BiChevronDown/>
           </Button>
-        </a>
+        </ButtonGroup> */}
 
         <a target="_blank" href="https://docs.baobun.dev">
-          <Button
-            variant="faded"
-            startContent={<BiBook className="text-lg" />}
-          >
+          <Button variant="faded" startContent={<BiBook className="text-lg" />}>
             Documentation
           </Button>
         </a>
